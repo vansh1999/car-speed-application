9aeb2e4bfaa555a70cdf8de6deb5bef6ccf41b9d
vansh bhardwaj <39709733+vansh1999@users.noreply.github.com>
2019-04-20 16:52:29 +0530

    Update carSpeed.js

--- carSpeed.js
+++ carSpeed.js
@@ -5,7 +5,7 @@ function initMovement(car,speed) {
   var carObj = document.getElementById(car)
   var left = 0
 
-  interval[car] = setInterval(function(){
+  interval.car = setInterval(function(){
 
        left += speed/100
        carObj.style.left = left + "px"
